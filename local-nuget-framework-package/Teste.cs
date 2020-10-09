123cf96196725427af34d959dcd4ebae4ec9d31d
Raywall Malheiros de Souza <raywall_malheiros@msn.com>
2020-10-09 13:18:22 -0300

    update

---
+++ local-nuget-framework-package/Teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_nuget_framework_package
+{
+    public class Teste
+    {
+        public static int Dobro(int valor) => valor * 2;
+    }
+}
